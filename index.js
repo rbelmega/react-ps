6099cd230d5973f7e1e61c2c58e65649a852f088
Belmeha, Rostyslav <RBELME06@iespc.ibm.com>
2016-07-29 17:38:10 +0300

    express settings

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require("path");
 
 app.use('/', express.static('public'));
 
+var renderIndex = (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public/index.html'));
+};
+
+app.get('/*', renderIndex);
 app.listen(process.env.PORT || 5000);
