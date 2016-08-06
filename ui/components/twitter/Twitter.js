836893dbff972fb4ef943d438709a1c183abd42d
Belmeha, Rostyslav <RBELME06@iespc.ibm.com>
2016-08-06 17:26:16 +0300

    twitter load

--- ui/components/twitter/Twitter.js
+++ ui/components/twitter/Twitter.js
@@ -1,6 +1,11 @@
 import React from "react"
+import {Timeline} from 'react-twitter-widgets'
 
 class Twitter extends React.Component {
+	constructor() {
+		super();
+		this.classLoaded = "";
+	}
 	getScript() {
 		var d = document;
 		var s = "script";
@@ -16,20 +21,40 @@ class Twitter extends React.Component {
 	}
 
 	componentDidMount() {
-		this.getScript();
+		// this.getScript();
+	}
+
+	loaded() {
+		this.classLoaded = "loaded";
+		this.forceUpdate()
 	}
 
 	render() {
+
 		return (
 			<div className="twitter">
-				<a className="twitter-timeline"
-					data-chrome="nofooter noheader noborder noscrollbar transparent"
-					href="https://twitter.com/izzz0"
-					data-theme="dark"
-					data-link-color="#000"
-					data-widget-id="694262144762822658">Tweets by @izzz0</a>
-				<div id="tweets"></div>
+				<div className={this.classLoaded}>
+					<Timeline
+						widgetId={'694262144762822658'}
+						options={{
+							username: 'izzz0',
+							chrome: "nofooter noheader noborder noscrollbar transparent",
+							theme: "dark",
+							linkColor: "#000"
+						}}
+						onLoad={() => this.loaded()}
+					/>
+				</div>
 			</div>
+			// <div className="twitter">
+			// 	<a className="twitter-timeline"
+			// 		data-chrome="nofooter noheader noborder noscrollbar transparent"
+			// 		href="https://twitter.com/izzz0"
+			// 		data-theme="dark"
+			// 		data-link-color="#000"
+			// 		data-widget-id="694262144762822658">Tweets by @izzz0</a>
+			// 	<div id="tweets"></div>
+			// </div>
 		);
 
 	}
