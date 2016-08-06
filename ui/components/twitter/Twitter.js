e9528f2a530a472e749dc505228b00415264d364
Belmeha, Rostyslav <RBELME06@iespc.ibm.com>
2016-08-06 18:05:50 +0300

    twitter load

--- ui/components/twitter/Twitter.js
+++ ui/components/twitter/Twitter.js
@@ -52,6 +52,18 @@ class Twitter extends React.Component {
 
 		return (
 			<div className="twitter">
+				{!this.classLoaded && <div className="sk-cube-grid">
+					<div className="sk-cube sk-cube1"></div>
+					<div className="sk-cube sk-cube2"></div>
+					<div className="sk-cube sk-cube3"></div>
+					<div className="sk-cube sk-cube4"></div>
+					<div className="sk-cube sk-cube5"></div>
+					<div className="sk-cube sk-cube6"></div>
+					<div className="sk-cube sk-cube7"></div>
+					<div className="sk-cube sk-cube8"></div>
+					<div className="sk-cube sk-cube9"></div>
+				</div>
+				}
 				<div className={this.classLoaded}>
 					<a className="twitter-timeline"
 						data-chrome="nofooter noheader noborder noscrollbar transparent"
