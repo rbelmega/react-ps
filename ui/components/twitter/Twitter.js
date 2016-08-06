5dbf6363da99072aeea97c14836bf6de8641f809
Belmeha, Rostyslav <RBELME06@iespc.ibm.com>
2016-08-06 17:40:25 +0300

    twitter load

--- ui/components/twitter/Twitter.js
+++ ui/components/twitter/Twitter.js
@@ -1,11 +1,12 @@
 import React from "react"
-import {Timeline} from 'react-twitter-widgets'
+// import {Timeline} from 'react-twitter-widgets'
 
 class Twitter extends React.Component {
 	constructor() {
 		super();
 		this.classLoaded = "";
 	}
+
 	getScript() {
 		var d = document;
 		var s = "script";
@@ -17,12 +18,30 @@ class Twitter extends React.Component {
 			js.id = id;
 			js.src = "http://platform.twitter.com/widgets.js";
 			fjs.parentNode.insertBefore(js, fjs);
+			js.onload = () => {
+				this.loaded();
+			};
 		}
 	}
 
 	componentDidMount() {
-		// this.getScript();
+		this.getScript();
 	}
+
+
+// <div className={this.classLoaded}>
+// <Timeline
+// widgetId={'694262144762822658'}
+// options={{
+// 	username: 'izzz0',
+// 	chrome: "nofooter noheader noborder noscrollbar transparent",
+// 	theme: "dark",
+// 	linkColor: "#000"
+// }}
+// onLoad={() => this.loaded()}
+// />
+// </div>
+// </div>
 
 	loaded() {
 		this.classLoaded = "loaded";
@@ -34,27 +53,15 @@ class Twitter extends React.Component {
 		return (
 			<div className="twitter">
 				<div className={this.classLoaded}>
-					<Timeline
-						widgetId={'694262144762822658'}
-						options={{
-							username: 'izzz0',
-							chrome: "nofooter noheader noborder noscrollbar transparent",
-							theme: "dark",
-							linkColor: "#000"
-						}}
-						onLoad={() => this.loaded()}
-					/>
+					<a className="twitter-timeline"
+						data-chrome="nofooter noheader noborder noscrollbar transparent"
+						href="https://twitter.com/izzz0"
+						data-theme="dark"
+						data-link-color="#000"
+						data-widget-id="694262144762822658">Tweets by @izzz0</a>
+					<div id="tweets"></div>
 				</div>
 			</div>
-			// <div className="twitter">
-			// 	<a className="twitter-timeline"
-			// 		data-chrome="nofooter noheader noborder noscrollbar transparent"
-			// 		href="https://twitter.com/izzz0"
-			// 		data-theme="dark"
-			// 		data-link-color="#000"
-			// 		data-widget-id="694262144762822658">Tweets by @izzz0</a>
-			// 	<div id="tweets"></div>
-			// </div>
 		);
 
 	}
